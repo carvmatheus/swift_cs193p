18af2b4fed253a604925e233e9040abe729f2e43
carvmatheus <mathcarvcard@gmail.com>
2024-04-11 19:33:16 -0300

    alteracao codigo para adicionar cartas

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by matheus cardoso on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
