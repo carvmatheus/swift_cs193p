670792fae0b182171557234378ad250bf997d790
carvmatheus <mathcarvcard@gmail.com>
2024-04-11 21:01:32 -0300

    finished lesson 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -13,7 +13,10 @@ struct ContentView: View {  // Struct => é qualquer coisa
     @State var cardCount: Int = 4
     var body: some View {   // Se é uma View, tem que ter esse elemento.
         VStack{
-            cards
+            ScrollView{
+                cards
+            }
+            Spacer()
             cardAdjusters
         }
         .padding()
@@ -27,11 +30,13 @@ struct ContentView: View {  // Struct => é qualquer coisa
             .font(.largeTitle)
     }
     var cards: some View{
-        HStack {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
-        }.foregroundColor(.orange)
+        }
+            .foregroundColor(.orange)
     }
     func cardModAdjusters(by offset: Int, symbol: String) -> some View {
         Button(action: {
@@ -56,13 +61,14 @@ struct CardView: View{
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill()
-            }
+            }.opacity(isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(isFaceUp ? 0: 1)
+            
         }.onTapGesture{
             isFaceUp.toggle()
         }
